e0c05d31972496b47bdf58111d46084bd00c95fa
Shiraz Besrour <50299287+Shiraz20@users.noreply.github.com>
2021-10-01 13:16:36 +0100

    components commit

---
+++ src/app/reclamation-list/reclamation-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reclamation-list',
+  templateUrl: './reclamation-list.component.html',
+  styleUrls: ['./reclamation-list.component.css']
+})
+export class ReclamationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
