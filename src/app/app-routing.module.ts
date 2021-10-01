de13d9f5f3766aa5035b4cb9e801d9178679c17e
Shiraz Besrour <50299287+Shiraz20@users.noreply.github.com>
2021-10-01 13:14:59 +0100

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccueilComponent} from "./accueil/accueil.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {CommonModule} from "@angular/common";
+import {BooksComponent} from "./books/books.component";
+import {ContactComponent} from "./contact/contact.component";
+import {LoginComponent} from "./login/login.component";
+import {AddBookComponent} from "./add-book/add-book.component";
+import {ReclamationListComponent} from "./reclamation-list/reclamation-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'accueil',
+    component: AccueilComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'accueil',
+    pathMatch: 'full',
+  },
+  {
+    path: 'books',
+    component: BooksComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'nosbook',
+    component: AddBookComponent,
+  },
+  {
+    path: 'rec-list',
+    component: ReclamationListComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { BooksComponent } from './books/books.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { ReclamationListComponent } from './reclamation-list/reclamation-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    BooksComponent,
+    ContactComponent,
+    LoginComponent,
+    AddBookComponent,
+    ReclamationListComponent
   ],
   imports: [
     BrowserModule,
